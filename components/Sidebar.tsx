afb9cbb35410cf91b0d59db73c9082eba3fc059d
lgomez <luis2021>
2026-02-18 14:48:14 -0500

    eliminar importación no utilizada en Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { BadgePlus, Menu, CirclePlus, Search, X } from 'lucide-react';
+import { BadgePlus, Menu, Search, X } from 'lucide-react';
 import { SidebarProps } from '../types/chat';
 import { getQuickActions, type QuickAction } from '../public/constants/quickActions';
 
